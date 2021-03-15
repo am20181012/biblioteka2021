0b89870b17b0f3dcbd6aa1ae4bfdbf24230baa27
am20181012 <andjelamijatovic98@gmail.com>
2021-03-15 15:44:02 +0100

    Dokumentovan interfejs BibliotekaInterfejs

--- src/biblioteka/interfejs/BibliotekaInterface.java
+++ src/biblioteka/interfejs/BibliotekaInterface.java
@@ -5,8 +5,22 @@ import java.util.LinkedList;
 import biblioteka.Autor;
 import biblioteka.Knjiga;
 
+/**
+ * Interfejs biblioteka
+ * 
+ * @author Andjela Mijatovic
+ *
+ */
 public interface BibliotekaInterface {
 	
+	/**
+	 * Dodaj novu knjigu u biblioteku.
+	 * 
+	 * @param knjiga Knjiga koja se dodaje.
+	 * 
+	 * @throws java.lang.NullPointerExceprion ako je uneta knjiga null
+	 * @throws java.lang.RuntimeExceprion ako uneta knjiga vec postoji u biblioteci(duplikat)
+	 */
 	public void dodajKnjigu(Knjiga knjiga);
 	
 	public void obrisiKnjigu(Knjiga knjiga);
