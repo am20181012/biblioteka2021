186024cecce7ffb7246fbfd0c16af89725b9cc52
am20181012 <andjelamijatovic98@gmail.com>
2021-03-15 13:07:27 +0100

    Dodata logicka kontrola u klasu Autor

--- src/biblioteka/Autor.java
+++ src/biblioteka/Autor.java
@@ -5,16 +5,36 @@ public class Autor {
 	private String ime;
 	private String prezime;
 	
+	public Autor() {
+		super();
+	}
+	public Autor(String ime, String prezime) {
+		super();
+		setIme(ime);
+		setPrezime(prezime);
+	}
 	public String getIme() {
 		return ime;
 	}
 	public void setIme(String ime) {
+		if(ime == null) {
+			throw new NullPointerException("Ime autora ne sme biti null.");
+		}
+		if(ime.length() < 2) {
+			throw new RuntimeException("Ime mora imati vise od 2 znaka.");
+		}
 		this.ime = ime;
 	}
 	public String getPrezime() {
 		return prezime;
 	}
 	public void setPrezime(String prezime) {
+		if(prezime == null) {
+			throw new NullPointerException("Prezime autora ne sme biti null.");
+		}
+		if(prezime.length() < 2) {
+			throw new RuntimeException("Prezime mora imati vise od 2 znaka.");
+		}
 		this.prezime = prezime;
 	}
 	@Override
